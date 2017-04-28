9c5bac39df7820dc3da7da7f69e0720d1cd879d2
orilme <1148269418@qq.com>
2017-04-28 18:09:20 +0800

    [1]第一次上传代码

---
+++ Sequence/Sequence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sequence
+//
+//  Created by orilme on 2017/4/24.
+//  Copyright © 2017年 orilme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sequence/Sequence/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sequence
+//
+//  Created by orilme on 2017/4/24.
+//  Copyright © 2017年 orilme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
